b243e3e4f29cd15edef78db38b13de51c86fa554
Eda Çolak <edaacolakk1907@gmail.com>
2025-04-29 19:20:21 +0300

    Edit

---
+++ src/db.java
@@ -0,0 +1,33 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eda
+ */
+public class db {
+    Connection conn=null;
+   
+   public static Connection connect_db()
+   {
+       try
+       {
+           Class.forName("org.sqlite.JDBC");
+          Connection  conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Eda\\Documents\\NetBeansProjects\\FINAL\\iks.sqlite");
+           return conn;
+       }
+       catch(Exception ex)
+       {
+           JOptionPane.showMessageDialog(null, ex);
+           return null; 
+      }
+   }
+}
